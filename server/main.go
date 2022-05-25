c477c59d7ad54ee124c7f14b33aef6d7eddc86f6
woongda <lgu4821@gmail.com>
2022-05-25 11:43:11 +0900

    구조 최조 제작 커밋

---
+++ server/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/KwangWoongLee/go_server/handler"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	fmt.Print("hi~")
+
+	router := gin.Default()
+	router.GET("/", handler.TestHandlerFunc)
+	router.Run(":80")
+}
